9f63e9d8efdcb97f9c5a8e988559e9c45a50e709
loretakorf <96435648+Loretakorf@users.noreply.github.com>
2023-04-09 14:35:31 +0300

    server

--- loginform/src/helpers/getUser.js
+++ loginform/src/helpers/getUser.js
@@ -1,14 +1,15 @@
-import { routes } from "../constants/routes"
+import { routes } from "../constants/routes";
+import Cookies from "js-cookie";
 
 export const getUser = () => {
-    // const token = Cookies.get("_todo_token");
-  
-    return fetch(routes.homePage, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      if (!response.ok) throw new Error("Request failed");
-      return response.json();
-    });
-  };
+  const token = Cookies.get("_todo_token");
+
+  return fetch(routes.homePage, {
+    headers: {
+      token,
+    },
+  }).then((response) => {
+    if (!response.ok) throw new Error("Request failed");
+    return response.json();
+  });
+};
